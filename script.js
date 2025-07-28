2b03e9439ea9effb06d01f66ce0528f8b946ef70
puja <proy87830@gmail.com>
2025-07-28 12:55:45 +0530

    update all

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const place = document.querySelector("input");
 const btn = document.querySelector("#btn");
 const feel = document.querySelector(".feel");
 const degree = document.querySelector(".degree");
-const iconImage = document.getElementById("weatherIcon");
+const iconImage = document.querySelector(".icon");
 
-btn.addEventListener("click", () => {
+function weather() {
   const loc = place.value.trim();
 
-  const weather = async () => {
+  const temp = async () => {
     try {
       const res = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=${loc}&aqi=no`
@@ -16,21 +16,35 @@ btn.addEventListener("click", () => {
       console.log(data);
       degree.innerHTML = `${data.current.temp_c}° C`;
       feel.innerHTML = `Feels like ${data.current.feelslike_c}° C`;
-      const condition = data.current.condition.text.toLowerCase();
-      const iconURL = data.current.condition.icon;
-      iconImage.src = iconURL;
+      // const condition = data.current.condition.text.toLowerCase();
+      // const iconURL = data.current.condition.icon;
+      // iconImage.src = iconURL;
 
-      let vgImg = "";
-      if (condition.includes("rain")) {
-        vgImg = "images/rain.jpg";
-      } else if (condition.includes("cloud")) {
-        vgImg = "images/cloudy.jpg";
-      } else {
-        vgImg = "images/patly.jpg";
+      switch (data.current.condition.text) {
+        case "Partly Cloudy":
+          iconImage.innerHTML = `<img src='./amcharts_weather_icons_1.0.0/animated/cloudy.svg' alt="weatherIcon" width="200"/>`;
+          break;
+        case "Mist":
+          iconImage.innerHTML = `<img src='./amcharts_weather_icons_1.0.0/animated/snowy-1.svg' alt="weatherIcon" width="200"/>`;
+          break;
+        case "Sunny":
+          iconImage.innerHTML = `<img src='./amcharts_weather_icons_1.0.0/animated/snowy-1.svg' alt="weatherIcon" width="200"/>`;
+          break;
+        case "Overcast ":
+          iconImage.innerHTML = `<img src='./amcharts_weather_icons_1.0.0/animated/rainy-1.svg' alt="weatherIcon" width="200"/>`;
+          break;
+          default:
+            iconImage.innerHTML = `<img src='./amcharts_weather_icons_1.0.0/animated/cloudy-day-3.svg' alt="weatherIcon" width="200"/>`;
       }
     } catch (error) {
-      console.log("Error fetching weather:", error);
+      (degree.innerHTML = "Please give a correct location"), error;
+      feel.innerHTML = "";
     }
   };
-  weather();
+  temp();
+}
+
+place.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") weather();
 });
+btn.addEventListener("click", weather);
